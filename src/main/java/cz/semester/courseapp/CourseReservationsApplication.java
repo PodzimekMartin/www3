63b5e083f7c73147be7144d3abd037c8f174a898
PodzimekMartin <126859478+PodzimekMartin@users.noreply.github.com>
2026-05-04 15:51:27 +0200

    chore: bootstrap spring boot build

---
+++ src/main/java/cz/semester/courseapp/CourseReservationsApplication.java
@@ -0,0 +1,12 @@
+package cz.semester.courseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseReservationsApplication.class, args);
+    }
+}
